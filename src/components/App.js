1067c72d7d5efd09a3ddabfffa08ddc7a013c22c
Shmigolk <shmigolftf@gmail.com>
2022-04-29 13:56:30 +0300

    added page state

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,29 @@
 import React from "react";
 import Character from "./Character";
 import '../App.css';
+import {nanoid} from "nanoid"
 
-function App() {
+export default function App() {
 
     const [characters, setCharacters] = React.useState([])
+    const [page, setPage] = React.useState(1)
+
     React.useEffect( () => {
         fetch("https://rickandmortyapi.com/api/character").then(res => res.json())
             .then(res => setCharacters(res.results))
     }, [])
 
+    function showCharPage(item){
+        console.log(item.location.name)
+    }
+
     let charactersRendering = characters.map( item => (
-        <Character characterData = {item}/>
-        ))
+        <Character
+            key = {nanoid()}
+            characterData = {item}
+            showCharPage = {() => showCharPage(item)}
+        />
+        )).slice(page * 3 - 3, page * 3)
 
     return (
       <div className ='character-container'>
@@ -21,4 +32,7 @@ function App() {
   )
 }
 
-export default App;
+
+
+
+
