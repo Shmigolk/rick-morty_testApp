068a4ea8ab7ad8d18e90694457b89a8c68fe8308
Shmigolk <shmigolftf@gmail.com>
2022-04-29 14:27:35 +0300

    working with pagination rendering

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import Character from "./Character";
 import '../App.css';
 import {nanoid} from "nanoid"
+import Pagination from "./pagination";
 
 export default function App() {
 
     const [characters, setCharacters] = React.useState([])
-    const [page, setPage] = React.useState(1)
+    const [currentPage, setCurrentPage] = React.useState(1)
+    const itemsPerPage = 3
 
     React.useEffect( () => {
-        fetch("https://rickandmortyapi.com/api/character").then(res => res.json())
+        fetch("https://rickandmortyapi.com/api/character")
+            .then(res => res.json())
             .then(res => setCharacters(res.results))
     }, [])
 
@@ -17,22 +20,34 @@ export default function App() {
         console.log(item.location.name)
     }
 
-    let charactersRendering = characters.map( item => (
+    const charactersRendering = characters.map( item => (
         <Character
             key = {nanoid()}
             characterData = {item}
             showCharPage = {() => showCharPage(item)}
         />
-        )).slice(page * 3 - 3, page * 3)
+        )).slice(currentPage * 3 - 3, currentPage * 3)
+
+    const pagination = getPaginationArray(characters, itemsPerPage)
+        .map( item => <Pagination pageNumber = {item}/>)
+
 
     return (
+        <div>
       <div className ='character-container'>
           {charactersRendering}
       </div>
+            <div className="pagination">{pagination}</div>
+        </div>
   )
 }
 
-
-
+function getPaginationArray(characters, itemsPerPage = 4){
+    let Arr = []
+    for (let page = 1; page <= characters.length / itemsPerPage - 1; page ++){
+        Arr.push(page)
+    }
+    return Arr
+}
 
 
--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Pagination(props) {
-
+    return (
+        <div>
+        <h1>{props.pageNumber}</h1>
+        </div>
+    )
 }
