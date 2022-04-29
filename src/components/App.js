dbe86b9c894e20d83773b2b120b238d9f4f4fbd4
Shmigolk <shmigolftf@gmail.com>
2022-04-29 11:37:51 +0300

    added slice

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,25 @@ import React from "react";
 import Character from "./Character";
 import '../App.css';
 
-
-
 function App() {
 
-    const [character, setCharacter] = React.useState()
+    const [characters, setCharacters] = React.useState([])
     React.useEffect( () => {
         fetch("https://rickandmortyapi.com/api/character").then(res => res.json())
-            .then(res => setCharacter(res.results))
+            .then(res => setCharacters(res.results))
+
     }, [])
 
-    console.log(character)
+    console.log(characters)
 
-
-    /*return (
-      <div>
-
-      <Character
-          characterData = {character}/>
+    let charactersRendering = characters.map( item => (
+        <Character characterData = {item}/>
+        )).slice(0, 3)
+    return (
+      <div className ='character-container'>
+          {charactersRendering}
       </div>
-  )*/;
+  )
 }
 
 export default App;
