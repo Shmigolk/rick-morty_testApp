60352a60c26adf5ed2253cfbba61997e7f165743
Shmigolk <shmigolftf@gmail.com>
2022-05-01 12:56:12 +0300

    working on the Filter component. Name filter added

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 export default function Pagination(props) {
+    const pageNumber = props.changePageNumber
+
     return (
         <div
             className="pagination__box"
-            onClick={props.changePageNumber}>
-            {props.pageNumber}
+            onClick={pageNumber}>
+            {pageNumber}
         </div>
     )
 }
