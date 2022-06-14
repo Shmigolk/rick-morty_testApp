6604b73cf55eba57f7b505746aec212230b89c9c
Shmigolk <75388692+Shmigolk@users.noreply.github.com>
2022-06-14 17:41:56 +0300

    design: changed the width of Character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -4,23 +4,24 @@ import {Context} from "../Context";
 
 export default function Character(props) {
 
-
     const {image, name, gender, status, id} = props.characterData
 
     return (
-                <section
-                    className="character-card character-card__hover"
-                    >
-                    <Link to={`/${id}`}>
-                        <div className="character-card__avatar">
-                            <img src={image} alt='some text' className='card-image'/>
-                        </div>
-                        <div className= 'character-cart-text'>
-                            <h2>{name}</h2>
-                            <p>Gender: {gender}</p>
-                            <p>Status: {status}</p>
-                        </div>
-                    </Link>
-                </section>
+        <section
+            className="character-card character-card__hover"
+        >
+            <Link to={`/${id}`}>
+                <div className="character-card__avatar">
+                    <img src={image}
+                         alt='some text'
+                         className='card-image'/>
+                </div>
+                <div className='character-cart-text'>
+                    <h2>{name}</h2>
+                    <p>Gender: {gender}</p>
+                    <p>Status: {status}</p>
+                </div>
+            </Link>
+        </section>
     )
 }
