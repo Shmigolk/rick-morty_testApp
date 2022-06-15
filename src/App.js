8c670cab17858f5b2f119bcc866420658c56b59c
Shmigolk <75388692+Shmigolk@users.noreply.github.com>
2022-06-15 12:31:38 +0300

    deploy: my first attempt to deploy my app :)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import CharactersFromAPI from './components/CharactersFromAPI'
 import SingleCharPage from "./components/SingleCharPage";
+
 import {Routes, Route} from "react-router-dom";
-import {Context} from "./Context";
-import React, {useContext} from "react";
+
+
+import React from "react";
 
 export default function App(){
-    const {currentPage} = useContext(Context)
+
     return (
         <div>
             <Routes>
                 <Route path={'/:characterId'} element={<SingleCharPage />} />
                 <Route exact path={`/`} element={<CharactersFromAPI />} />
             </Routes>
+
         </div>)
 }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
+export default function Footer(){
+    return (
+        <footer>
+
+
+        </footer>
+    )
+}
